5badfbc37b46b8023aaca1d2e694818237bfcee3
Salavat Galiamov <sgaliamov@users.noreply.github.com>
2020-12-24 01:40:15 +0100

    Bb8 (#3)

--- dotnet/NotesApi/Benchmark/Program.cs
+++ dotnet/NotesApi/Benchmark/Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Dapper;
+using Npgsql;
 
 namespace Benchmark
 {
-    public class Note
-    {
-        public int id { get; set; }
-        public string text { get; set; }
-    }
-
     public sealed class Program
     {
         public static void Main()
@@ -24,41 +21,89 @@ namespace Benchmark
         [RankColumn]
         public class Benchmarks
         {
-            private const int Count = 500;
-            private static readonly HttpClient Client = new HttpClient();
+            private const string ConnectionString =
+                "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=postgres;Pooling=true;";
 
-            private static async Task Step(string url)
+            private const int Count = 100;
+            private static readonly HttpClient Client = new HttpClient();
+            private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(4);
+
+            public static async Task Step(string url)
             {
+                await Semaphore.WaitAsync(CancellationToken.None);
+
                 var post = await Client
                     .PostAsync(url, new StringContent(Guid.NewGuid().ToString()))
                     .ConfigureAwait(false);
 
-                var posted = await JsonSerializer
-                    .DeserializeAsync<Note>(
-                        await post.Content
-                            .ReadAsStreamAsync()
-                            .ConfigureAwait(false))
-                    .ConfigureAwait(false);
+                var posted = await post.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var id = posted.Split("\"id\":").Last().Split(",").First();
+                if (!string.IsNullOrWhiteSpace(id))
+                {
+                    var note = await Client.GetStringAsync($"{url}/{id}").ConfigureAwait(false);
+                    if (note != posted)
+                    {
+                        throw new InvalidOperationException($"get: {note}\npost: {posted}");
+                    }
+                }
 
-                var get = await Client.GetStreamAsync($"{url}/{posted.id}").ConfigureAwait(false);
-
-                var note = await JsonSerializer
-                    .DeserializeAsync<Note>(get)
-                    .ConfigureAwait(false);
-
-                if (note.text != posted.text) throw new InvalidOperationException();
+                Semaphore.Release();
             }
 
             [Benchmark]
-            public void Rust()
+            public void R2D2_Parallel() => RunInParallel(_ => Step("http://localhost:8080"));
+
+            [Benchmark]
+            public void BB8_Parallel() => RunInParallel(_ => Step("http://localhost:9080"));
+
+            [Benchmark]
+            public void Asp_Parallel() => RunInParallel(_ => Step("http://localhost:5000"));
+
+            [Benchmark]
+            public void R2D2_Sequence()
             {
-                for (var i = 0; i < Count; i++) Step("http://localhost:8080").GetAwaiter().GetResult();
+                RunInSequence("http://localhost:8080");
             }
 
             [Benchmark]
-            public void Asp()
+            public void BB8_Sequence()
             {
-                for (var i = 0; i < Count; i++) Step("http://localhost:5000").GetAwaiter().GetResult();
+                RunInSequence("http://localhost:9080");
+            }
+
+            [Benchmark]
+            public void Asp_Sequence()
+            {
+                RunInSequence("http://localhost:5000");
+            }
+
+            public static void RunInParallel(Func<int, Task> selector)
+            {
+                var tasks = Enumerable.Range(0, Count).AsParallel().Select(selector).ToArray();
+                Task.WaitAll(tasks);
+            }
+
+            [GlobalSetup]
+            public void GlobalSetup()
+            {
+                using var connection = new NpgsqlConnection(ConnectionString);
+
+                connection.Execute("drop table if exists notes");
+                Thread.Sleep(1000);
+                connection.Execute(@"
+                    create table if not exists notes (
+                        id serial,
+                            text varchar not null,
+                        timestamp timestamptz default now() not null)");
+                Thread.Sleep(1000);
+            }
+
+            private static void RunInSequence(string url)
+            {
+                for (var i = 0; i < Count; i++)
+                {
+                    Step(url).GetAwaiter().GetResult();
+                }
             }
         }
     }
---
+++ src/bin/bb8.rs
@@ -0,0 +1,139 @@
+#![feature(async_closure)]
+
+use actix_web::{
+    get,
+    http::StatusCode,
+    post,
+    web::{self, Path},
+    App, Error, HttpResponse, HttpServer, Result,
+};
+use bb8::Pool;
+use bb8_postgres::{tokio_postgres::NoTls, PostgresConnectionManager};
+use chrono::{DateTime, Utc};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Note {
+    id: i32,
+    text: String,
+    timestamp: DateTime<Utc>,
+}
+
+#[get("/{id}")]
+async fn get_note(
+    Path(id): Path<i32>,
+    db: web::Data<Pool<PostgresConnectionManager<NoTls>>>,
+) -> Result<HttpResponse, Error> {
+    match db
+        .get_ref()
+        .run(async move |cl| {
+            let prepare = cl
+                .prepare(
+                    "select id, text, timestamp
+                    from notes
+                    where id = $1",
+                )
+                .await;
+
+            match prepare {
+                Ok(select) => match cl.query_one(&select, &[&id]).await {
+                    Ok(one) => {
+                        let id: i32 = one.get("id");
+                        let text: String = one.get("text");
+                        let timestamp: DateTime<Utc> = one.get("timestamp");
+                        let note = Note {
+                            id,
+                            text,
+                            timestamp,
+                        };
+                        Ok((note, cl))
+                    }
+                    Err(e) => Err((e, cl)),
+                },
+                Err(e) => Err((e, cl)),
+            }
+        })
+        .await
+    {
+        Ok(x) => Ok(HttpResponse::Ok().json(x)),
+        Err(e) => {
+            println!("{}", e);
+            Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR))
+        }
+    }
+}
+
+#[post("/")]
+async fn add_note(
+    text: String,
+    db: web::Data<Pool<PostgresConnectionManager<NoTls>>>,
+) -> Result<HttpResponse, Error> {
+    match db
+        .get_ref()
+        .run(async move |cl| {
+            let prepare = cl
+                .prepare("insert into notes (text) values ($1) returning id, timestamp")
+                .await;
+
+            match prepare {
+                Ok(select) => match cl.query_one(&select, &[&text]).await {
+                    Ok(one) => {
+                        let id: i32 = one.get("id");
+                        let timestamp: DateTime<Utc> = one.get("timestamp");
+                        let note = Note {
+                            id,
+                            text,
+                            timestamp,
+                        };
+                        Ok((note, cl))
+                    }
+                    Err(e) => Err((e, cl)),
+                },
+                Err(e) => Err((e, cl)),
+            }
+        })
+        .await
+    {
+        Ok(x) => Ok(HttpResponse::Ok().json(x)),
+        Err(e) => {
+            println!("{}", e);
+            Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR))
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let manager = PostgresConnectionManager::new(
+        "host=localhost user=postgres port=5432 password=postgres"
+            .parse()
+            .unwrap(),
+        NoTls,
+    );
+    let pool = Pool::builder().build(manager).await.unwrap();
+    let client = pool.get().await.unwrap();
+    client
+        .execute(
+            "create table if not exists notes (
+                id serial,
+                text varchar not null,
+                timestamp timestamptz default now() not null
+            )",
+            &[],
+        )
+        .await
+        .unwrap();
+
+    println!("Listening on http://localhost:9080");
+
+    let pool = pool.clone();
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(get_note)
+            .service(add_note)
+    })
+    .bind("127.0.0.1:9080")?
+    .run()
+    .await
+}
