2682023b5505579a53612b48ee203d4dc8103bbd
s. galiamov <sgalyamov@gmail.com>
2020-12-24 01:39:44 +0100

    done

--- README.md
+++ README.md
@@ -23,7 +23,11 @@ Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical 
   DefaultJob : .NET Core 5.0.1 (CoreCLR 5.0.120.57516, CoreFX 5.0.120.57516), X64 RyuJIT
 ```
 
-| Method |    Mean |    Error |   StdDev |  Median | Rank |
-|------- |--------:|---------:|---------:|--------:|-----:|
-|   Rust | 8.525 s | 0.1662 s | 0.1848 s | 8.468 s |    1 |
-|    Asp | 8.956 s | 0.1578 s | 0.1318 s | 8.931 s |    2 |
+| Method        |     Mean |    Error |   StdDev |   Median |
+| ------------- | -------: | -------: | -------: | -------: |
+| Asp_Parallel  | 223.9 ms |  9.87 ms | 29.11 ms | 226.0 ms |
+| R2D2_Parallel | 241.9 ms |  4.80 ms |  4.25 ms | 242.1 ms |
+| BB8_Parallel  | 283.3 ms |  5.64 ms | 10.31 ms | 284.5 ms |
+| R2D2_Sequence | 904.4 ms | 17.89 ms | 44.89 ms | 910.2 ms |
+| BB8_Sequence  | 966.2 ms | 18.56 ms | 18.22 ms | 964.7 ms |
+| Asp_Sequence  | 511.6 ms | 20.43 ms | 60.23 ms | 519.0 ms |
--- dotnet/NotesApi/Benchmark/Program.cs
+++ dotnet/NotesApi/Benchmark/Program.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Dapper;
+using Npgsql;
 
 namespace Benchmark
 {
-    public class Note
-    {
-        public int id { get; set; }
-        public string text { get; set; }
-    }
-
     public sealed class Program
     {
         public static void Main()
@@ -25,59 +21,33 @@ namespace Benchmark
         [RankColumn]
         public class Benchmarks
         {
-            private const int Count = 300;
-            private static readonly HttpClient Client = new HttpClient();
+            private const string ConnectionString =
+                "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=postgres;Pooling=true;";
 
-            private static async Task Step(string url)
+            private const int Count = 100;
+            private static readonly HttpClient Client = new HttpClient();
+            private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(4);
+
+            public static async Task Step(string url)
             {
+                await Semaphore.WaitAsync(CancellationToken.None);
+
                 var post = await Client
                     .PostAsync(url, new StringContent(Guid.NewGuid().ToString()))
                     .ConfigureAwait(false);
 
-                var posted = await JsonSerializer
-                    .DeserializeAsync<Note>(
-                        await post.Content
-                            .ReadAsStreamAsync()
-                            .ConfigureAwait(false))
-                    .ConfigureAwait(false);
-
-                var get = await Client.GetStreamAsync($"{url}/{posted.id}").ConfigureAwait(false);
-
-                var note = await JsonSerializer
-                    .DeserializeAsync<Note>(get)
-                    .ConfigureAwait(false);
-
-                if (note.text != posted.text)
+                var posted = await post.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var id = posted.Split("\"id\":").Last().Split(",").First();
+                if (!string.IsNullOrWhiteSpace(id))
                 {
-                    throw new InvalidOperationException();
+                    var note = await Client.GetStringAsync($"{url}/{id}").ConfigureAwait(false);
+                    if (note != posted)
+                    {
+                        throw new InvalidOperationException($"get: {note}\npost: {posted}");
+                    }
                 }
-            }
 
-            [Benchmark]
-            public void R2D2()
-            {
-                for (var i = 0; i < Count; i++)
-                {
-                    Step("http://localhost:8080").GetAwaiter().GetResult();
-                }
-            }
-
-            [Benchmark]
-            public void BB8()
-            {
-                for (var i = 0; i < Count; i++)
-                {
-                    Step("http://localhost:9080").GetAwaiter().GetResult();
-                }
-            }
-
-            [Benchmark]
-            public void Asp()
-            {
-                for (var i = 0; i < Count; i++)
-                {
-                    Step("http://localhost:5000").GetAwaiter().GetResult();
-                }
+                Semaphore.Release();
             }
 
             [Benchmark]
@@ -89,10 +59,51 @@ namespace Benchmark
             [Benchmark]
             public void Asp_Parallel() => RunInParallel(_ => Step("http://localhost:5000"));
 
-            private static void RunInParallel(Func<int, Task> selector)
+            [Benchmark]
+            public void R2D2_Sequence()
+            {
+                RunInSequence("http://localhost:8080");
+            }
+
+            [Benchmark]
+            public void BB8_Sequence()
+            {
+                RunInSequence("http://localhost:9080");
+            }
+
+            [Benchmark]
+            public void Asp_Sequence()
+            {
+                RunInSequence("http://localhost:5000");
+            }
+
+            public static void RunInParallel(Func<int, Task> selector)
             {
                 var tasks = Enumerable.Range(0, Count).AsParallel().Select(selector).ToArray();
                 Task.WaitAll(tasks);
+            }
+
+            [GlobalSetup]
+            public void GlobalSetup()
+            {
+                using var connection = new NpgsqlConnection(ConnectionString);
+
+                connection.Execute("drop table if exists notes");
+                Thread.Sleep(1000);
+                connection.Execute(@"
+                    create table if not exists notes (
+                        id serial,
+                            text varchar not null,
+                        timestamp timestamptz default now() not null)");
+                Thread.Sleep(1000);
+            }
+
+            private static void RunInSequence(string url)
+            {
+                for (var i = 0; i < Count; i++)
+                {
+                    Step(url).GetAwaiter().GetResult();
+                }
             }
         }
     }
