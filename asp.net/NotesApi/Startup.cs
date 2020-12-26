00623e9be5504444d4da18e52af96a54e1ef13b3
s. galiamov <sgalyamov@gmail.com>
2020-12-26 16:19:54 +0100

    moved files

--- asp.net/NotesApi/Startup.cs
+++ asp.net/NotesApi/Startup.cs
@@ -16,14 +16,12 @@ namespace NotesApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging(c => c.ClearProviders());
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
